cb59d1250c31db9bc9fcb419dcf5d0ca1313515e
Harshit Choudhary <hchoudhary1102@gmail.com>
2026-03-17 12:23:53 +0530

    Revert "Update main.py"
    This reverts commit 92aba2d2b6190f1c85f7dc3c05128f9a3b7a6493.

--- doctalk_backend/backend/main.py
+++ doctalk_backend/backend/main.py
@@ -121,10 +121,4 @@ app = create_app()
 
 if __name__ == "__main__":
     import uvicorn
-    import os
-
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8000)),
-    )
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
